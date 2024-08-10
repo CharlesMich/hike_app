3a545ec994c004d393c6b8f7cc42036e1ad0b326
CharlesMich <charleedongo@hotmail.com>
2024-08-10 12:51:29 -0500

    first commit

---
+++ hike/Component/CardView.swift
@@ -0,0 +1,65 @@
+//
+//  CardView.swift
+//  hike
+//
+//  Created by Charles Michael on 8/9/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            CustomBackgroundView()
+            VStack {
+//                Header
+                VStack(alignment:.leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size:52))
+                            .foregroundStyle(
+                                LinearGradient(colors: [.customGrayLight, .customGrayMedium],
+                                               startPoint: .top,
+                                           endPoint: .bottom)
+                            )
+                        Spacer()
+                        
+                        Button {
+                            print("The button was pressed")
+                        } label: {
+                           CustomButtonView()
+                        }
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
+//                main content
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors:
+                                [Color("ColorIndigoMedium"),
+                                 Color("ColorSalmonLight")], 
+                                startPoint:
+                                .topLeading, endPoint: .bottomTrailing)
+                            )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+//                footer
+            }
+        }
+        .frame(width: 320, height: 570)
+    }
+}
+
+#Preview {
+    CardView()
+}
