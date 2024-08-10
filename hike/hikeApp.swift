d16b29b5bb9b03445956eccc1be6246ce54ba557
CharlesMich <charleedongo@hotmail.com>
2024-08-09 20:58:16 -0500

    Initial Commit

---
+++ hike/hikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikeApp.swift
+//  hike
+//
+//  Created by Charles Michael on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct hikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
