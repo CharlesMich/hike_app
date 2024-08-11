9009611f56665d72e3ffc89d19f04bf403eeebca
CharlesMich <charleedongo@hotmail.com>
2024-08-11 15:22:33 -0500

    aug 11

--- hike/Component/MotionAnmationView.swift
+++ hike/Component/MotionAnmationView.swift
@@ -23,10 +23,17 @@ struct MotionAnmationView: View {
         return CGFloat(Int.random(in: 4...80))
     }
 //    Random Scale
-    
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
 //    Random Speed
+    func randomSpeed()-> Double {
+        return Double.random(in: 0.05...1.0)
+    }
 //    Random Delay
-    
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
     
     var body: some View {
         ZStack {
@@ -39,10 +46,13 @@ struct MotionAnmationView: View {
                         x: randomCoordinate(),
                         y: randomCoordinate()
                         )
+                    .scaleEffect(isAnimating ? randomScale() : 1)
                     .onAppear(perform: {
                         withAnimation (
                             .interpolatingSpring(stiffness: 0.25, damping: 0.25)
                             .repeatForever()
+                            .speed(randomSpeed())
+                            .delay(randomDelay())
                         ) {
                             isAnimating = true
                         }
@@ -51,12 +61,17 @@ struct MotionAnmationView: View {
            
         }
         .frame(width: 256, height: 256)
+        .mask(Circle())
+        .drawingGroup()
     }
 }
 
 #Preview {
     ZStack {
-        Color.teal.ignoresSafeArea()
+//        Color.teal.ignoresSafeArea()
         MotionAnmationView()
+            .background(
+            Circle()
+                .fill(.teal))
     }
 }
